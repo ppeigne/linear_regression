58fa259d43705cdbc7b8edaf348a3e554198e940
ppeigne <ppeigne@student.42.fr>
2019-08-13 16:04:02 +0200

    Add linear_regression class, main, and data files

---
+++ main.py
@@ -0,0 +1,34 @@
+from Classes.linear_regression import LinearRegression
+import numpy as np
+from matplotlib import pyplot as plt
+
+if __name__ == "__main__":
+    df = np.genfromtxt('data.csv', delimiter=',')
+    df = df[1:]
+    X, y = df[:, 0], df[:, 1]
+   # print(X)
+ #   X = np.stack((np.ones(X.shape[0]), X, X**2), axis=1)
+
+
+    #X = np.stack((X, X**2), axis=1)
+
+    #print(X.shape)
+
+    #X = np.stack([np.ones(X.size), X], axis=1)
+
+    l1 = LinearRegression(X, y, np.zeros(2))
+    res = l1.regularized_gradient_descent(0.5)
+    print(l1.theta)
+   # p = l1.predict([5000, 5000**2])
+    p = l1.predict([5000])
+    print(p)
+    data = np.genfromtxt('ex1data2.txt', delimiter=',')
+    #data = np.loadtxt(os.path.join('Data', 'ex1data2.txt'), delimiter=',')
+    X = data[:, :2]
+    y = data[:, 2]
+    l2 = LinearRegression(X, y, np.zeros(3))
+    res = l2.regularized_gradient_descent(0.1)
+    print(l2.theta)
+    p = l2.predict([1650, 3])
+    #p = l1.predict([5000])
+    print(p)
