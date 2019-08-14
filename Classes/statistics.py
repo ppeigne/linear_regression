0a53ea87e933f65ecfeb50c2255823fc202bb67b
ppeigne <ppeigne@student.42.fr>
2019-08-14 14:42:24 +0200

    Add Statistics class

---
+++ Classes/statistics.py
@@ -0,0 +1,69 @@
+import math
+
+
+class Statistics:
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.len = len(self.dataset)
+        self.total = self.sum()
+        self.mean = self.total / self.len
+        self.median = self.median_measure()
+        self.mode = self.mode_measure()
+        self.min = min(dataset)
+        self.max = max(dataset)
+        self.range = self.max - self.min
+        self.variance = self.variance_measure()
+        self.std = math.sqrt(self.variance)
+
+    def sum(self):
+        total = 0
+        for x in self.dataset:
+            total += x
+        return total
+
+    def median_measure(self):
+        sorted_set = sorted(self.dataset)
+        if self.len % 2 is 0:
+            middle = self.len / 2
+        else:
+            middle = int(self.len / 2 + 0.5)
+        return sorted_set[int(middle)]
+
+    def mode_measure(self):
+        count = {}
+        for x in self.dataset:
+            if x not in count:
+                count[x] = 1
+            else:
+                count[x] += 1
+        number = 1
+        mode = []
+        for v, c in count.items():
+            if number < c:
+                number = c
+                mode = [v]
+            elif number == c and c > 1:
+                mode.append(v)
+        if not mode:
+            mode = None
+        return mode
+
+    def variance_measure(self):
+        total = 0
+        for x in self.dataset:
+            total += (x - self.mean)**2
+        return total / (self.len - 1)
+
+    def print_stat(self):
+        print("----------------------")
+        print(sorted(self.dataset))
+        print("Mean = %s" % self.mean)
+        print("Median = %s" % self.median)
+        print("Mode = %s" % self.mode)
+        print("Min = %s" % self.min)
+        print("Max = %s" % self.max)
+        print("Range = %s" % self.range)
+        print("Variance = %s" % self.variance)
+        print("Std = %s" % self.std)
+        print("----------------------")
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from Classes.linear_regression import LinearRegression
+from Classes.statistics import Statistics
+
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -16,16 +18,32 @@ if __name__ == "__main__":
 
     #X = np.stack([np.ones(X.size), X], axis=1)
 
+    s1 = Statistics(X)
+    s1.print_stat()
+
     l1 = LinearRegression(X, y, np.zeros(2))
     res = l1.regularized_gradient_descent(0.5)
     print(l1.theta)
    # p = l1.predict([5000, 5000**2])
     p = l1.predict([5000])
     print(p)
+
+
+
+
+
     data = np.genfromtxt('ex1data2.txt', delimiter=',')
     #data = np.loadtxt(os.path.join('Data', 'ex1data2.txt'), delimiter=',')
     X = data[:, :2]
     y = data[:, 2]
+
+    s2 = Statistics(X.T[0])
+    s2.print_stat()
+
+    s3 = Statistics(X.T[1])
+    s3.print_stat()
+
+
     l2 = LinearRegression(X, y, np.zeros(3))
     res = l2.regularized_gradient_descent(0.1)
     print(l2.theta)
