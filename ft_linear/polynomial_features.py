9428f34c9488e9715b798cd7bdc41f73a5cdaccb
ppeigne <ppeigne@student.42.fr>
2020-09-24 20:56:34 +0200

    add comments in polynomial_features

--- ft_linear/polynomial_features.py
+++ ft_linear/polynomial_features.py
@@ -8,18 +8,12 @@ class PolynomialFeatures():
         self.interaction_only = interaction_only
         self.include_bias = include_bias
     
-    def rm_duplicate(self, src, df):
+    def __rm_duplicate__(self, src, df):
         src_len = src.shape[1]
         df_len = df.shape[1]
-    #    print(src.shape)
-    #    print(df.shape)
-        rm = 0
         for i in range(src_len - rm):
             j = 0
             while j < df_len:
-              #  print(j)
-              #  print(df.shape)
-              #  print(df_len, end="\n\n")
                 if np.array_equal(src[:,i],df[:,j]):
                     df = np.delete(df, j, 1)
                     df_len = df.shape[1]
@@ -27,17 +21,14 @@ class PolynomialFeatures():
         return df
 
     def transform(self, df):
-        res = np.copy(df)
-        tmp = np.copy(res)
+        tmp = np.copy(df)
         for i in range(1, self.degree):
-            res = np.copy(tmp)#, axis=1)
+            res = tmp
             for j in range(df.shape[1]):
-                tmp = np.concatenate((tmp, self.rm_duplicate(tmp, res[:,j].reshape(-1,1) * res[:,j:])), axis=1)
-                #tmp = self.rm_duplicate(res, res[:,j].reshape(-1,1) * res[:,j:])#), axis=1)
-                
-                print(i,j)
-                print(tmp, end="\n\n")
-        print(tmp)
+                new_block = res[:,j].reshape(-1,1) * res[:,j:] # generate a new block of cols from res[:,j] times what remains in the df
+                new_block = self.__rm_duplicate__(tmp, new_block) # remove diplicated cols from the new block 
+                tmp = np.concatenate((tmp, new_block), axis=1)                
+        return tmp
 
 X = np.arange(6).reshape(3, 2)
 print(X, end="\n\n")
